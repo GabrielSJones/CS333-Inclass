b0bc6551bdf843d687596b5ce98138f4d57315fd
James T <james.taylor314@u.boisestate.edu>
2026-02-02 13:01:16 -0700

    added for-loop and case statement to main method

--- main.sh
+++ main.sh
@@ -2,7 +2,22 @@
 
 main(){
 	echo "This is just a test"
-    source ArgumentEcho.sh "$@" # need to pass the arguments over
+    source ArgumentEcho.sh "$*" # need to pass the arguments over
+
+    #allows for multiple  requests to be handled in a single input
+    for arg in "$@" #space delimed args
+    do 
+        case "$arg" in
+            "-h" | "help")
+            printf "\nYou have asked for help"
+            ;;
+
+            "-p" | "ping" | "pingsweep")
+            printf "\nYou have asked to perform a ping sweep"
+            ;;
+        esac
+    done
+
 }
 
 main "$@" #must be included to actually call main otherwise nothing will happen
